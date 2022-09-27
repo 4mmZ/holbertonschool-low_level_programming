6d2495a99618a4eb33b54ed8d715f28635792d3a
Andres <andresmaxi111014@gmail.com>
2022-09-27 11:57:59 -0700

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongType));
-	printf("Size of a float: %ld byte(s)\n", sizeof(floatType);
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 	return (0);
-}
+	}
