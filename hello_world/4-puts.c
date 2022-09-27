8800c6d8ccfcf56ed24523ae3bcf4513687ee8aa
Andres <andresmaxi111014@gmail.com>
2022-09-27 09:23:33 -0700

    task 4.9

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main (void);
+int main ();
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-
-	return 0;
+	char str1[51];
+	
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	
+	puts(str1);
+	
+	return(0);
 }
