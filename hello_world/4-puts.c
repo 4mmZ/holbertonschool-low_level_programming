f8cd5ef39553a17d6eff48e281d4b93b1aa7bb9c
Andres <andresmaxi111014@gmail.com>
2022-09-27 09:08:28 -0700

    task 4.6

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gets -E  | puts ("Programming is like building a multilingual puzzle\
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle")
+
+	return 0;
+}
