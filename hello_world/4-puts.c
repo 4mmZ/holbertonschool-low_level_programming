c28f4de54f15dd69f6381afc9584bd4d51bcdfb9
Andres <andresmaxi111014@gmail.com>
2022-09-27 09:09:48 -0700

    task 4.6

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,9 +2,9 @@
 #include <conio.h>
 #include <stdlib.h>
 
-int main (void)
+int main (void);
 {
-	puts ("\"Programming is like building a multilingual puzzle")
+	puts ("\"Programming is like building a multilingual puzzle");
 
 	return 0;
 }
