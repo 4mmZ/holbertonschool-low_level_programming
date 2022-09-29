f127a115427734b21aa0a68fa0289370faaf4710
Andres <andresmaxi111014@gmail.com>
2022-09-29 09:14:02 -0700

    task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,10 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	{
 	if (letter != 'q' && letter != 'e')
 		putchar(letter);
-
+	}
 	putchar(10);
 	return (0);
 }
