42ab92bd8d9a46c6fa418385f74f19b2a414078e
Andres <andresmaxi111014@gmail.com>
2022-09-30 11:31:12 -0700

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ *print_alphabet - i wanna die
+ *Return: Always (0)
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{	
+		_putchar(ch);
+	}
+	_putchar(10)
+}
