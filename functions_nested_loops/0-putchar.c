85f306a4d21aeb9f2830e7cebad6dcd7a52b43a6
Andres <andresmaxi111014@gmail.com>
2022-09-30 09:21:45 -0700

    task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main - prit
+ * Return= 0 (Succes)
+ */
+int main (void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	putchar(10);
+	return(0)
+}
