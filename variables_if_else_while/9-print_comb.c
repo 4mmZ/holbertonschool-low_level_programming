e4aeda12f18f4547f7cbab67f617208f60c76790
Andres <andresmaxi111014@gmail.com>
2022-09-30 06:46:13 -0700

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ * Return: return 0
+ */
+
+int main(void)
+
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+		if (n < 9)
+		{
+			putchar(',')
+			putchar(' ')
+		}
+
+	putchar('\n');
+	return (0);
+}
+
