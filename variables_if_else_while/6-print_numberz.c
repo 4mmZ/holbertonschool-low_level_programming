4602a4bf540971db1bca254190a0fa7c35528f2d
Andres <andresmaxi111014@gmail.com>
2022-09-30 06:10:17 -0700

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ * Return: return 0
+ */
+
+int main(void)
+
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+
+	putchar('\n');
+	return (0);
+}
+
