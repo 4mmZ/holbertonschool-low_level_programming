b80c611c75aa2b5abcae314dac7b4463eb8e9a8d
Andres <andresmaxi111014@gmail.com>
2022-10-01 12:50:59 -0700

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - i print the aBCdariou ten times
+ *Return: Always (0)
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		ch = 'a';
+		while ('a' <= 'z')
+		_putchar(ch);
+	}
+	_putchar(10);
+	n++;
+}
