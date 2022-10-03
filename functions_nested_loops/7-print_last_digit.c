87bf30bcff48d58e915338be868c875d28a95fab
Andres <andresmaxi111014@gmail.com>
2022-10-03 07:23:21 -0700

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - fuction
+ * @num: number use
+ * Return: digit
+ */
+int print_last_digit(int num)
+{
+	int digit;
+
+	if (num < 0)
+		num = -num;
+	digit = num % 10;
+
+	if (digit < 0)
+		digit = -digit;
+	_putchar(digit + '0');
+
+	return (digit);
+}
