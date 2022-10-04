52b9e85307cb0a5c0035ccac46aa4b33663628b8
Andres <andresmaxi111014@gmail.com>
2022-10-04 11:22:33 -0700

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_most_numbers - 0 to 9
+ *Return: succes except 2 and 4
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9' && n != '2' && n != '4'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
+}
