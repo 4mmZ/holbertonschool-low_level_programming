6c3ec012e1b2e17c55e51d78960d081b24e4a6d6
Andres <andresmaxi111014@gmail.com>
2022-10-05 04:28:30 -0700

    task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,8 +7,9 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9' && n != '2' && n != '4'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
+		if (n != '2' && n != '4')
 		_putchar(n);
 	}
 	_putchar(10);
