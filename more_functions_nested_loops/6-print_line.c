7b2bbc38bb21383adf703dbb10408f56e850f8c8
Andres <andresmaxi111014@gmail.com>
2022-10-05 07:12:48 -0700

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_line - a line
+ *@n: numbers of time that the barritau will be ecxecuted
+ *Return: none
+ */
+void print_line(int n);
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		--n;
+	}
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+}
