29330fd2cfe68bd96f127f6e9fc8892d97b60b04
Andres <andresmaxi111014@gmail.com>
2022-10-05 10:42:39 -0700

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,13 @@ int main(void)
 		{
 			printf("FizzBazz");
 		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
 		}
 		else
 		{
