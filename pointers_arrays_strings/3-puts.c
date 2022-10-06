e9f8d5b412331663d43ab4cb6959a45f61228f84
Andres <andresmaxi111014@gmail.com>
2022-10-06 09:55:12 -0700

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts - char
+ *@str: an string
+ *Return: nothing
+*/ 
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str = str + 1;
+	}
+	_putchar(10);
+}
