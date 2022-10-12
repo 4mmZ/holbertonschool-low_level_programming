a4863a0c6398c52220c2a14e33c1a56a5ec49a30
Andres <andresmaxi111014@gmail.com>
2022-10-12 09:30:54 -0700

    task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,33 @@
 #include "main.h"
-/**
-* puts2 - print the string
-*
-*
-*
-*/
- 
- void puts2(char *str)
-{
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+/**
+ * puts2 - fucion
+ * @str: variable
+ * Return: always
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int cont = _strlen(str);
+
+	for (; i < cont; i += 2)
 	{
 		_putchar(str[i]);
 	}
-	_putchar(10);
+	_putchar('\n');
+}
+/**
+ * _strlen - fucion
+ * @s: variable
+ * Return: always
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
 }
