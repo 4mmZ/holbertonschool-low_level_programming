60b8e4beae3bc00d9e93d0b7b1f597e9b0e95a49
Andres <andresmaxi111014@gmail.com>
2022-10-12 06:23:46 -0700

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	int c = 0;
+	int len = 0;
+
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+	len = (c + 1) / 2;
+	while (len < c)
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar(10);
+}
