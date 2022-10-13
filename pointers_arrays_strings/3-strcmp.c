33e1920964ce8529d501fd5ed90ac55280ebbfbe
Andres <andresmaxi111014@gmail.com>
2022-10-13 06:39:10 -0700

    task 3. 1

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,18 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int n = 0;
 
-	for (; s1[i] != '\0' && s2[n] != '\0';)
+	for (; s1[i] != '\0' && s2[i] != '\0';)
 	{
-		if (s1[i] - s2[n] < 0)
+		if (s1[i] - s2[i] < 0)
 		{
-			return (s1[i] - s2[n]);
+			return (s1[i] - s2[i]);
 		}
-		else if (s1[i] - s2[n] > 0)
+		else if (s1[i] - s2[i] > 0)
 		{
-			return (s1[i] - s2[n]);
+			return (s1[i] - s2[i]);
 		}
-		else if (s1[i] - s2[n] == 0)
+		else if (s1[i] - s2[i] == 0)
 		{
 			return (0);
 		}
