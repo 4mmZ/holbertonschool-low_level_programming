53a7e9ecd2c0f35cc54e06a02fa3aaec8600db8d
Andres <andresmaxi111014@gmail.com>
2022-10-14 08:15:41 -0700

    task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts_recursion - the recursion
+ *@s: the pointer
+ *Return: (void)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0' )
+	{
+		_putchar('\n');
+			return;
+	}
+	_putchar(s[0]);
+	s++;
+	_puts_recursion(s);
+}
