1ab3c505f9a21c30f5ee97c12888f5746743e7fa
Andres <andresmaxi111014@gmail.com>
2022-10-18 07:56:56 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - the principal function
+ *@argc: number of arguments
+ *@argv: what argc contains
+ *Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	if (argc)
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
