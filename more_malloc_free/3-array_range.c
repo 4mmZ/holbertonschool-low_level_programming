34edec4dde486a541e57480e35360418b287adb6
Andres <andresmaxi111014@gmail.com>
2022-10-26 08:31:07 -0700

    task 3 fix

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *arrat_range - the function that return the range of a array
+ *array_range - the function that return the range of a array
  *@min: the min value
  *@max: the max value
  *Return: a
@@ -16,12 +16,17 @@ int *array_range(int min, int max)
 	{
 		l = max - min + 1;
 
-		a = malloc(sizeof(int) * (max - min + 1));
-
+		if (min > max)
+		{
+			return (NULL);
+		}
 		if (a == NULL)
 		{
 			return (NULL);
 		}
+
+		a = malloc(sizeof(int) * (max - min + 1));
+
 		for (i = 0; i < l; i++)
 		{
 			a[i] = min;
