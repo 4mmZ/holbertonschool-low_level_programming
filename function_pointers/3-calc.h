d379e90772e18c3b879fd671a9eae8201a403766
Andres <andresmaxi111014@gmail.com>
2022-11-01 06:44:23 -0700

    task 3 prototype

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,5 +1,8 @@
+#ifndef calc_h
+#define calc_h
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -18,3 +21,4 @@ typedef struct op
     char *op;
     int (*f)(int a, int b);
 } op_t;
+#endif
--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -9,5 +9,24 @@
 
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	while (i <= 5)
+	{
+		strcmp(s, ops[i].op) == 0;
+			return(ops[i].op);
+	}
+	return(NULL);
+
 
 }
+
