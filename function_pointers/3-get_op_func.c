c70b3dd91d783ae15e40b679d4d9150f174c49a4
Andres <andresmaxi111014@gmail.com>
2022-11-01 12:57:34 -0700

    task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -20,9 +20,9 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (i < 5)
+	while (*s && ops[i].op)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (!strcmp(s, ops[i].op))
 		{
 			return (ops[i].f);
 		}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,23 +9,19 @@
 
 int main(int argc, char *argv[])
 {
-	int num2;
 	int (*f)(int a, int b);
-
-	num2  = atoi(argv[3]);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	f  = get_op_func(argv[2]);
-	if ((*argv[2] == '/' && num2 == 0) || (*argv[2] == '%' && num2 == 0))
+	f = get_op_func(argv[2]);
+	if (*argv[3] == '0')
 	{
 		printf("Error\n");
 		exit(100);
 	}
 	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
-
 	return (0);
 }
