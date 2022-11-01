3b82945150097143a5d72aff0034a8736cfc9580
Andres <andresmaxi111014@gmail.com>
2022-11-01 12:28:39 -0700

    pesao

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,11 +10,6 @@
 int main(int argc, char *argv[])
 {
 	int (*f)(int a, int b);
-	int num1;
-	int num2;
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 
 	if (argc != 4)
 	{
@@ -22,13 +17,13 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	f  = get_op_func(argv[2]);
-	if ((*argv[2] == '/' && num2 == 0) || (*argv[2] == '%' && num2 == 0))
+	if ((*argv[2] == '/' && *argv[3] == 0) || (*argv[2] == '%' && *argv[3] == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", f(num1, num2));
+	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
 	
 	return(0);
 }
