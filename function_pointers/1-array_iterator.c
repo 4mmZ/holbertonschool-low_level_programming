f4b7e04a6546d6a60e4b96f96564f10a5318801a
Andres <andresmaxi111014@gmail.com>
2022-11-01 12:04:45 -0700

    task 0,1,2

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,3 +1,23 @@
-#include <>
-
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - function
+ * @array: array(integer)
+ * @size: size of array
+ * @action: pointer of function
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (action)
+	{
+		if (array)
+
+			for (i = 0; i < size; i++)
+		{
+			(action)(*array);
+			array++;
+		}
+	}
+}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,23 +9,26 @@
 
 int main(int argc, char *argv[])
 {
+	int (*f)(int a, int b);
 	int num1;
 	int num2;
 
-	if (argc)
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (argc != 4)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
-
-		if ((*argv[2] == '/' && num2 == 0) || (*argv[2] == '%' && num2 == 0))
-		{
-			printf("Error\n");
-			exit(100);
-		}
-
-		printf("%d\n", get_op_func(argv[2])(num1, num2));
-
+		printf("Error\n");
+		exit(98);
 	}
-	return (0);
+	f  = get_op_func(argv[2]);
+	if ((*argv[2] == '/' && num2 == 0) || (*argv[2] == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
+	printf("%d\n", f(num1, num2));
+	
+	return(0);
 }
