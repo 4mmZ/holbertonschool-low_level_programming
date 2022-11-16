6d3e4ae5886667d27c80c3e399f44e2385a8766b
Andres <andresmaxi111014@gmail.com>
2022-11-16 08:45:58 -0800

    linked list directory

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ *print_list - function that print the contents of a list
+ *@list_t: structure that conatins the list
+ *@h: the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t j = 0;
+
+	while (h)
+	{
+		if (h->str)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nill)\n");
+		}
+
+		h = h->next;
+		j++;
+	}
+	
+	return(j);
+}
