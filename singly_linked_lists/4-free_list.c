b5d3de24832c7700b23d8be83a35dc741a631131
Andres <andresmaxi111014@gmail.com>
2022-11-18 04:44:44 -0800

    task 4 test

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *free_list - functin that free a linked list
+ *@head the start of the linked list
+ *Return: void
+ */
+
+void free_list(list_t *head)
+{
+
+	while (head)
+	{
+		free_list(head->next);
+
+		if (head->str)
+		{
+			free(head->str);
+		}
+	}
+	free(head);
+}
