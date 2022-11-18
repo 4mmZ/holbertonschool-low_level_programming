376bf75a937c06d1f759ee5ab554d14a1d5675df
Andres <andresmaxi111014@gmail.com>
2022-11-18 07:15:55 -0800

    function that adds a node at the end of a linked list

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,44 +1,14 @@
 #include "lists.h"
-list_t *add_node(list_t **head, const char *str);
-/**
- *add_node_end - function that add a node at the end of the list
- *@head: the pointer to the start of the list
- *@str: the string that runs the string
- *Return: last A.K.A the new node sited in the end of the loist
- */
-
-list_t *add_node_end(list_t **head, const char *str)
-{
-	list_t *endnode = NULL;
-	list_t *last = *head;
-
-	if (last == NULL)
-	{
-		*head = add_node(head, str);
-		return (*head);
-	}
-	while (last->next != NULL)
-	{
-		last = last->next;
-	}
-
-	endnode = add_node(head, str);
-	last->next = endnode;
-	last = endnode;
-
-
-	return (last);
-}
 
 /**
-*add_node - function that add a node in the begginin
+*add - function that add a node in the begginin
 *@head: the first node of the list
 *@str: the string
 *Return: newnode
  */
 
 
-list_t *add_node(__attribute((unused)) list_t **head, const char *str)
+list_t *add(const char *str)
 {
 
 	list_t *newnode = NULL;
@@ -55,4 +25,33 @@ list_t *add_node(__attribute((unused)) list_t **head, const char *str)
 		newnode->next = NULL;
 	return (newnode);
 
+}
+
+/**
+ *add_node_end - function that add a node at the end of the list
+ *@head: the pointer to the start of the list
+ *@str: the string that runs the string
+ *Return: last A.K.A the new node sited in the end of the loist
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *last = *head;
+	list_t *endnode = NULL;
+
+	if (last == NULL)
+	{
+		*head = add(str);
+		return (*head);
+	}
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	endnode = add(str);
+	last->next = endnode;
+
+
+	return (last->next);
 }
