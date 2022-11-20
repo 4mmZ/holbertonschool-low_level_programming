0b52e5669490d0855dfe95cd088d9ec8c2881397
Andres <andresmaxi111014@gmail.com>
2022-11-20 12:51:20 -0800

    function that frees a list and set the head to NULL

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -2,9 +2,9 @@
 #include "4-free_listint.c"
 
 /**
- *
- *
- *
+ *free_listint2 - function that frees a list and set the head to NULL
+ *@head: the list
+ *Return: void
  */
 
 void free_listint2(listint_t **head)
