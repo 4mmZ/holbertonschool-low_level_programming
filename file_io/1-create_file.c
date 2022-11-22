6217ac6fe686b86c381b5627160a83090f657440
Andres <andresmaxi111014@gmail.com>
2022-11-22 05:16:41 -0800

    function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,6 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 		file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	return (1);
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
