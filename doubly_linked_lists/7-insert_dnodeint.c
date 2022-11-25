2f63730fe38eb9ef306f468e7a4db7772b06067e
Andres <andresmaxi111014@gmail.com>
2022-11-25 11:44:33 -0800

    test me quiero ir

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,10 +2,11 @@
 #include "5-get_dnodeint.c"
 
 /**
- *
- *
- *
- *
+ *insert_dnodeint_at_index - function that insert a node at the given position.
+ *@h: the pointer to the pointer to the first node
+ *@idx: index
+ *@n: integrers
+ *Return: the new node
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
@@ -18,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	index = get_dnodeint_at_index(*h, idx - 1);
 	if (index == NULL)
-		return(NULL);
+		return (NULL);
 	newnode = add_dnodeint(&(index->next), n);
 	newnode->prev = index;
 	return (newnode);
