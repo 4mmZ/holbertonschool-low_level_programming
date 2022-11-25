40f116b6c393d3348431408b8fcb3d136a4f52be
Andres <andresmaxi111014@gmail.com>
2022-11-25 11:38:37 -0800

    test me quiero ir

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,6 +20,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (index == NULL)
 		return(NULL);
 	newnode = add_dnodeint(&(index->next), n);
-
+	newnode->prev = index;
 	return (newnode);
 }
