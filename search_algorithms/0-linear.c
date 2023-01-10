81c5e9dea62b3da2d39d0d57f86b5e2b73726ff1
Andres <andresmaxi111014@gmail.com>
2023-01-10 14:56:42 -0800

    search algortim binary and linear for task 0 and 1

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,6 +1,14 @@
 #include "search_algos.h"
 #include <stddef.h>
 
+/**
+ *linear_search - function that searches for a value in an array of integers
+ using the Linear search algorithm
+ *@array: is a pointer to the first element of the array to search in
+ *@size: is the number of elements in array
+ *@value: is the value to search for
+ *Return: The checked value if is in the array or -1 if it fail
+ */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t count;
@@ -11,7 +19,7 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", count, array[i]);
 		if (array[i] == value)
 		{
-                        return (count);
+			return (count);
 		}
 		i++;
 	}
--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 
+/**
+ *binary_search - function that searches for a value in a sorted
+ *array of integers using the Binary search algorithm
+ *@array:  is a pointer to the first element of the array to search in
+ *@size: is the number of elements in array
+ *@value: is the value to search for
+ *Return: The checked value if is in the array or -1 if it fail
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
@@ -25,5 +33,5 @@ int binary_search(int *array, size_t size, int value)
 		else
 			low = mid + 1;
 	}
-	return -1;
+	return (-1);
 }
